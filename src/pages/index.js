e10a6feb29c24831d7cf86a00f991e7fc5550150
Cezar Sampaio <cezarsmpio@gmail.com>
2022-09-19 13:05:25 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,7 @@ export const query = graphql`
         cover {
           gatsbyImageData(
             jpegProgressive: true
-            quality: 100
+            quality: 90
             width: 900
             height: 900
             cropFocus: CENTER
